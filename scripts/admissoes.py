6dc742153ba0d7f7936f22b318600c45e24cfba7
Gabriel <gaabrielgomes3241@gmail.com>
2024-12-04 16:00:31 -0300

    Implementação do analisador de imagens

--- scripts/admissoes.py
+++ scripts/admissoes.py
@@ -3,6 +3,7 @@ import os
 import pyautogui
 import time
 
+guidance_images_dir = r'C:\Users\Gabriel\Desktop\analises_escritorio\import\guidance_images'
 
 time.sleep(3)
 pyautogui.dragTo(x=180, y=90)
@@ -10,7 +11,7 @@ time.sleep(0.5)
 pyautogui.click(x=180, y=90)
 while True:
     try:
-        pyautogui.locateOnScreen(image='images/cadastro_empregados.png', confidence=0.6)
+        pyautogui.locateOnScreen(image=(os.path.join(guidance_images_dir, "cadastro_empregados.png")), confidence=0.6)
         print("Janela aberta.")
         break
     except pyautogui.ImageNotFoundException:
